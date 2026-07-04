2e3b1b7175e4395702c7f4a79aa165842f30e140
JCOrisakwe <oriz4work@gmail.com>
2026-07-04 12:37:40 +0100

    feat: implement typing logic with indicators and scroll sync

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+// TEXT BOX LAYOUT AND FUNCTIONALITY
+const indicators = document.querySelector(".validity-indicators");
+const promptContainer = document.querySelector(".prompt-container");
+let roundOver = false;
+let charIndex = 0;
+
+const getBottomDistance = (el) => el.getBoundingClientRect().bottom;
+const pcBottomDistance = getBottomDistance(promptContainer);
+const pcLineHeight = Number(
+  getComputedStyle(promptContainer).lineHeight.replace("px", ""),
+);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key.length === 1 && !roundOver) {
+    // adding validity indicator
+    const indicator = document.createElement("span");
+    indicator.textContent = "!";
+
+    if (promptContainer.innerText[charIndex] === e.key) {
+      indicator.classList.add("correct-char");
+    } else {
+      indicator.classList.add("wrong-char");
+    }
+    indicators.append(indicator);
+    charIndex += 1;
+    roundOver = indicators.children.length >= promptContainer.innerText.length;
+
+    // textbox scroll down mechanism
+    if (getBottomDistance(indicator) > pcBottomDistance) {
+      indicators.scrollTop += pcLineHeight;
+    }
+    // removing validity indicator
+  } else if (e.key.toLowerCase() === "backspace") {
+    const lastIndicator = indicators.children[indicators.children.length - 1];
+    if (lastIndicator) lastIndicator.remove();
+    charIndex = Math.max(0, charIndex - 1);
+  }
+  promptContainer.scrollTop = indicators.scrollTop;
+});
+
+// KEYBOARD LAYOUT AND FUNCTIONALITY
 const keyRows = document.querySelectorAll(".key-row");
 let capsLockActive = false;
 
@@ -20,6 +61,7 @@ document.addEventListener("keydown", (e) => {
   }
 
   if (keyText === "capslock") {
+    if (e.repeat) return;
     capsLockActive = !capsLockActive;
     keyRows.forEach((row) =>
       row.classList.toggle("capslock-down", capsLockActive),
